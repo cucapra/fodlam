55c34c69f844aedaeba57e60f1eabb8ddf72c9d3
Adrian Sampson <adrian@radbox.org>
2017-11-08 17:25:48 -0500

    Nicer parsing of EIE data

--- fodlam.py
+++ fodlam.py
@@ -8,10 +8,15 @@ EYERISS_FILE = 'eyeriss-vgg16.csv'
 def load_data():
     """Load the published numbers from our data files.
     """
+    eie_vgg_latencies = {}
     with open(os.path.join(DATA_DIR, EIE_DATA)) as f:
         reader = csv.DictReader(f)
         for row in reader:
-            print(row)
+            if row['Layer'] == 'Actual Time':
+                for k, v in row.items():
+                    if k.startswith('VGG16'):
+                        eie_vgg_latencies[k] = v
+    print(eie_vgg_latencies)
 
     with open(os.path.join(DATA_DIR, EYERISS_FILE)) as f:
         reader = csv.DictReader(f)
