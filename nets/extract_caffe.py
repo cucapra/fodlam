64b79ad56d7ddf518847dd33f4b36fe65d20788c
Adrian Sampson <adrian@radbox.org>
2017-11-14 17:41:09 -0500

    Trivial JSON conversion

--- nets/extract_caffe.py
+++ nets/extract_caffe.py
@@ -1,25 +1,13 @@
 #!/usr/bin/env python3
 import google.protobuf.text_format
+import google.protobuf.json_format
 import caffe_pb2
 import sys
-import json
-
-
-def convert_layer(layer):
-    return {
-        "name": layer.name,
-    }
-
 
 def convert(infile):
     text = infile.read()
     net = google.protobuf.text_format.Merge(text, caffe_pb2.NetParameter())
-    return {
-        "name": net.name,
-        "layers": [ convert_layer(l) for l in net.layers ]
-    }
-
+    return google.protobuf.json_format.MessageToJson(net)
 
 if __name__ == '__main__':
-    out = convert(sys.stdin)
-    print(json.dumps(out, indent=2, sort_keys=True))
+    print(convert(sys.stdin))
