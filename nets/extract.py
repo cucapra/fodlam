73d6aba722e6484ab188a0e37fdacd800a325a30
Adrian Sampson <adrian@radbox.org>
2017-11-15 17:58:42 -0500

    Use Mark's MAC counting code

--- nets/extract.py
+++ nets/extract.py
@@ -5,7 +5,32 @@ import sys
 import caffe
 
 def extract(model_fn):
+    # Load the model from the prototxt file.
     net = caffe.Net(model_fn, caffe.TEST)
+
+    for name, layer in zip(net._layer_names, net.layers):
+        print(name, layer)
+        if layer.type in ('Convolution', 'Deconvolution'):
+            # Get the input blob for this layer and its parameters (weights).
+            blob = net.blobs[net.top_names[name][0]]
+            weights = net.params[name][0]
+
+            # Extract relevant hyperparameters from the layer's
+            # activation and weights.
+            layer_height = blob.shape[2]
+            layer_width = blob.shape[3]
+            in_chan = weights.shape[0]
+            out_chan = weights.shape[1]
+            kernel_height = weights.shape[2]
+            kernel_width = weights.shape[3]
+
+            # Compute the total number of multiply-and-accumulate operations
+            # for this convolutional layer.
+            num_outputs = layer_width * layer_height * out_chan
+            num_macs_per_out = in_chan * kernel_height * kernel_width
+            num_macs = num_outputs * num_macs_per_out
+
+            print(name, num_macs)
 
 if __name__ == '__main__':
     extract(sys.argv[1])
